279d596a435abd56014db087d695d54fba9081ec
Keith Flynn <flynn.keith@gmail.com>
2023-12-06 15:43:04 -0500

    Results moved to DOM from console
    DOM now updates win/loss/tie count.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,92 +1,83 @@
-
+"use-strict";
 
 // Scorekeeping
 let wins = 0;
 let losses = 0;
 let ties = 0;
-let announceScore = `Wins: ${wins} Losses: ${losses} Ties: ${ties}.`
 
-// Assign 1, 2, 3 to rock, paper, scissors and RNG to roll a result
+// Assign 1, 2, 3 to Rock, Paper, Scissors and RNG to roll a result
 function getComputerChoice() {
   // roll 1-3
   let computerRoll = Math.floor(Math.random() * 3) + 1;
-  console.log(computerRoll);
 
-  // assign roll to string value of rock/paper/scissors and return
+  // assign roll to string value of Rock/Paper/Scissors and return
   let computerSelection = "";
   if (computerRoll === 1) {
-    computerSelection = "rock";
+    computerSelection = "Rock";
   } else if (computerRoll === 2) {
-    computerSelection = "paper";
+    computerSelection = "Paper";
   } else {
-    computerSelection = "scissors";
+    computerSelection = "Scissors";
   }
 
   return computerSelection;
 }
 
-// Prompt user to choose rock, paper, or scissors
-function getPlayerChoice() {
-  // ask promt user to choose, save it as lowercase
-  let rawPlayerSelection = prompt("Rock, paper, or scissors?");
-  let playerSelection = rawPlayerSelection.toLowerCase();
+// Run a single round of a Rock, Paper, Scissors match
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
 
-  // make sure player gives an actual anser:
-  if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
-    return playerSelection;
-  } else {
-    alert("Invalid choice! Please choose either rock, paper, or scissors.");
-    return getPlayerChoice();
-  }
-
-}
-
-// Run a single round of a rock, paper, scissors match
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock") {
-    if (computerSelection === "rock") {
+  if (playerSelection === "Rock") {
+    if (computerSelection === "Rock") {
       ties++;
       console.log("Tie.");
-
-    } else if (computerSelection === "paper") {
+    } else if (computerSelection === "Paper") {
       losses++;
       console.log("You lose.");
     } else {
       wins++;
       console.log("You win.");
     }
-  } else if (playerSelection === "paper") {
-    if (computerSelection === "rock") {
+  } else if (playerSelection === "Paper") {
+    if (computerSelection === "Rock") {
       wins++;
       console.log("You win.");
-    } else if (computerSelection === "paper") {
+    } else if (computerSelection === "Paper") {
       ties++;
       console.log("Tie.");
-
     } else {
       losses++;
       console.log("You lose.");
     }
   } else {
-    if (computerSelection === "rock") {
+    if (computerSelection === "Rock") {
       losses++;
       console.log("You lose.");
-    } else if (computerSelection === "paper") {
+    } else if (computerSelection === "Paper") {
       wins++;
       console.log("You win.");
     } else {
       ties++;
-      console.log("Tie")
-
+      console.log("Tie");
     }
   }
   updateScore();
 }
 
-const buttons = document.querySelectorAll('button');
+// Called at the end of playRound() to update variable
+function updateScore() {
+  const resultsDiv = document.getElementById('results')
+  resultsDiv.replaceChildren();
+  const scoreboard = document.createElement('h2')
+  scoreboard.innerText = `Wins: ${wins} Losses: ${losses} Ties: ${ties}.`;
+  resultsDiv.appendChild(scoreboard);
+}
+
+// Button event listeners
+const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    console.log(button.innerText);
+  button.addEventListener("click", () => {
+    playRound(button.innerHTML);
   });
 });
